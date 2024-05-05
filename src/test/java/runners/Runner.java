07de127b9d5e39d4e8ab08e7cf976fb6a093d1d8
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-05 21:05:13 +0300

    deneme

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = {"src/test/resources/features"},
         glue = {"stepdefinitions", "hooks"},
+
         tags = "@API",
         dryRun = false
 )
