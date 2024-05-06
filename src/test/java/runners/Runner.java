a69e18006e91110458ff06c7eacb20e568b794af
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-06 16:29:49 +0300

    SUCCESS CODE

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,6 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = {"src/test/resources/features"},
-
         glue = {"stepdefinitions", "hooks"},
         tags = "@API",
         dryRun = false
