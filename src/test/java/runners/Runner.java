be0e58a7a3597361c7a7a7bfea9f26fa6aff0b10
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-06 12:08:58 +0300

    HATASIZ KOD OLMAZ

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = {"src/test/resources/features"},
-        glue = {"stepdefinitions", "hooks"},
+        gue = {"stepdefinitions", "hooks"},
         tags = "@API",
         dryRun = false
 )
